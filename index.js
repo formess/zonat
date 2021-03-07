3dd5dc3794e2a9e477dfc138c7b273bc382fbd31
wattroll <wattroll@protonmail.com>
2021-03-07 13:49:06 +0200

    Use @mapbox/togeojson directly and render zones with style

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
 import Leaflet from "leaflet";
 import "./node_modules/leaflet/dist/leaflet.css";
-import LeafletOmnivore from "leaflet-omnivore";
+import { kml as parseKml } from 'togeojson';
+import CartoColor from 'cartocolor';
+
+const paveMid = "1XPZjR9bKFqduKwRkjHUECEhmcwhO53dW";
+const isoRatasMid = "1ZrmW-kxq4VK-ND9Hj6kWlcuD3-z18mBB";
 
 const map = addMap(document.body);
 addRasterTiles(map);
-addIsoRatasLayer(map);
+
+withGoogleMapsKmlDocument(isoRatasMid, function(err, xml) {
+  if (!xml) {
+    if (err)
+      console.error(err);
+    throw new Error("Failed to load map document");
+  }
+  const kml = parseKml(xml);
+  const delivery = deliveryZonesLayer(kml);
+  delivery.addTo(map);
+  map.setMaxBounds(delivery.getBounds().pad(0.05));
+  map.setMinZoom(map.getBoundsZoom(delivery.getBounds()));
+  map.setZoom(map.getMinZoom());
+  map.panInsideBounds(delivery.getBounds());
+});
+
+function deliveryZonesLayer(deliveryZones) {
+  const zonesLayer = Leaflet.geoJSON(deliveryZones)
+  const zoneLayers = zonesLayer.getLayers();
+  const colors = CartoColor.Sunset[zoneLayers.length];
+  zoneLayers.forEach(function(zoneLayer, index) {
+    const style = {
+      weight: 3,
+      color: colors[index],
+      fillOpacity: 0.5
+    }
+    zoneLayer.on({
+      mouseover: function(event) {
+        zoneLayer.setStyle({
+          weight: 6,
+          fillOpacity: 0.3,
+        });
+        zoneLayer.bringToFront();
+      },
+      mouseout: function(event) {
+        zoneLayer.setStyle(style);
+      },
+      click: function(event) {
+        if (zoneLayer._map)
+          zoneLayer._map.fitBounds(zoneLayer.getBounds());
+      }
+    })
+    zoneLayer.setStyle(style);
+  })
+  return zonesLayer;
+}
 
 function addMap(element) {
   if (!element) { element = document.body };
@@ -16,7 +65,10 @@ function addMap(element) {
 
   const map = Leaflet.map(div, {
     center: helsinkiCoordinates,
-    zoom: 13
+    zoom: 12,
+    zoomSnap: 0.5,
+    maxBoundsViscosity: 0.75,
+    renderer: Leaflet.svg({ padding: 1 })
   });
   return map;
 }
@@ -28,18 +80,26 @@ function addRasterTiles(map) {
   tileLayer.addTo(map);
 }
 
-function addPaveLayer(map) {
-  addGoogleMapsKmlLayer(map, '1XPZjR9bKFqduKwRkjHUECEhmcwhO53dW');
-}
-
-function addIsoRatasLayer(map) {
-  addGoogleMapsKmlLayer(map, '1ZrmW-kxq4VK-ND9Hj6kWlcuD3-z18mBB');
-}
-
-function addGoogleMapsKmlLayer(map, mid) {
-  LeafletOmnivore.kml(googleMapsKmlUrl(mid)).addTo(map);
-}
-
 function googleMapsKmlUrl(mid) {
   return `https://www.google.com/maps/d/kml?forcekml=1&mid=${mid}`
+}
+
+function withGoogleMapsKmlDocument(mid, callback) {
+  const url = googleMapsKmlUrl(mid);
+  const request = new XMLHttpRequest;
+  request.open('GET', url);
+  request.send();
+  withXhrXmlResponse(request, callback);
+  return request;
+}
+
+function withXhrXmlResponse(request, callback) {
+  if (request.status === 200 && request.responseXML)
+    callback(null, request.responseXML);
+  // TODO: try parsing `request.responseText`
+  else if (request.readyState !== (request.readyState.DONE || 4))
+    request.addEventListener("loadend", () => withXhrXmlResponse(request, callback));
+  else
+    callback(request, null);
+  return request;
 }

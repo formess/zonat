8190d827d7f16c27de48b179fc56f8e0f3a942be
wattroll <wattroll@protonmail.com>
2021-04-18 14:40:13 +0300

    HelNinjaTileLayer: set default map center and zoom if it's not already set

--- src/Helsinki.js
+++ src/Helsinki.js
@@ -1,4 +1,5 @@
 import { TileLayer } from 'leaflet/src/layer/tile/TileLayer';
+import { LatLng } from 'leaflet/src/geo/LatLng';
 
 // Leaflet TileLayer with raster tiles from tiles.hel.ninja
 // See https://dev.hel.fi/maps and http://tiles.hel.ninja/ for details
@@ -12,6 +13,10 @@ export var HelNinjaTileLayer = TileLayer.extend({
   initialize(options = {}) {
     const tileUrl = helNinjaTileUrl(options);
     TileLayer.prototype.initialize.call(this, tileUrl, options);
+  },
+  beforeAdd: function(map) {
+    setMapCenter(map, centerLatLng);
+    return TileLayer.prototype.beforeAdd.call(this, map);
   }
 });
 
@@ -19,4 +24,13 @@ export function helNinjaTileUrl(options = {}) {
   const style = options.style || "hel-osm-bright"
   const lang = options.language || options.lang || "fi";
   return `https://tiles.hel.ninja/styles/${style}/{z}/{x}/{y}{r}@${lang}.png`;
+}
+
+export var centerLatLng = new LatLng(60.192059, 24.945831);
+
+function setMapCenter(map, center) {
+  if (!map.options.center || !map._loaded) {
+    map.options.center = center;
+    map.setView(center, map.options.zoom || 11, { reset: true })
+  }
 }

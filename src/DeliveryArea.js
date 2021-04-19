1e11e411cabfbbb21d53a6159223e76bd2703f70
wattroll <wattroll@protonmail.com>
2021-04-19 09:20:38 +0300

    Add DeliveryAreaLegend

--- src/DeliveryArea.js
+++ src/DeliveryArea.js
@@ -2,6 +2,9 @@ import { Map } from 'leaflet/src/map';
 import { SVG, Polygon } from 'leaflet/src/layer/vector';
 import { LayerGroup } from 'leaflet/src/layer/LayerGroup';
 import { GeoJSON } from 'leaflet/src/layer/GeoJSON';
+import { Control } from 'leaflet/src/control';
+import { create as createElement, addClass, removeClass } from 'leaflet/src/dom/DomUtil';
+import { on as onDomEvent } from 'leaflet/src/dom/DomEvent';
 
 export var DeliveryAreaMap = Map.extend({
   options: {
@@ -49,6 +52,44 @@ export var DeliveryArea = GeoJSON.extend({
       color: zoneLayer.feature.properties.fill,
       fillOpacity: state === 'focused' ? 0.2 : 0.5
     }
+  }
+});
+
+export var DeliveryAreaLegend = Control.extend({
+  options: { position: 'topright' },
+  initialize(zonesLayer, options) {
+    this.zonesLayer = zonesLayer;
+    Control.prototype.initialize.call(this, options);
+  },
+  onAdd(map) {
+    const legendDiv = createElement('div', 'legend');
+    if (this.options.title) {
+      const legendTitle = createElement('h4', 'legend__title', legendDiv);
+      legendTitle.textContent = this.options.title;
+    }
+    this.zonesLayer.getLayers().forEach((zoneLayer, index) => {
+      const name = zoneLayer.feature.properties.name;
+      const color = !zoneLayer.getLayers
+            ? zoneLayer.options.color
+            : zoneLayer.getLayers()[0].options.color;
+      const zoneDiv = createElement('div', 'legend__zone', legendDiv);
+      const colorSpan = createElement('span', 'legend__zone-color', zoneDiv);
+      colorSpan.style.background = color;
+      const nameSpan = createElement('span', 'legend__zone-name', zoneDiv);
+      nameSpan.textContent = name;
+      onDomEvent(zoneDiv, {
+        click() { map.flyToBounds(zoneLayer.getBounds()) },
+        mouseover() { zoneLayer.fireEvent('mouseover') },
+        mouseout() { zoneLayer.fireEvent('mouseout') },
+        focus() { zoneLayer.fireEvent('mouseover') },
+        blur() { zoneLayer.fireEvent('mouseout') },
+      });
+      zoneLayer.on({
+        mouseover() { addClass(zoneDiv, "legend__zone--focused") },
+        mouseout() { removeClass(zoneDiv, "legend__zone--focused") },
+      });
+    });
+    return legendDiv;
   }
 });
 

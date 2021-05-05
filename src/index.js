12932c08b6335f8a0881455a844dc704b7f5f13f
wattroll <wattroll@protonmail.com>
2021-05-05 22:27:55 +0300

    Add forgotten index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+export {
+  DeliveryAreaMap,
+  DeliveryAreaData,
+} from './DeliveryArea';
+export {
+  Search,
+  DigiTransitSearch,
+} from './Search';
+export {
+  withMapData,
+} from './GoogleMyMaps';
+export {
+  HelNinjaTileLayer,
+  helNinjaTileUrl,
+} from './HelNinja';
